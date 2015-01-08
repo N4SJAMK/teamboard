01106fcdf2c10157de2f4b8f122ad55d04bf85a9
tanelih <taneli.hartikainen@jamk.fi>
2015-01-08 14:40:28 +0200

    Rework initialization script to support forking

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/N4SJAMK/teamboard-io     io
-git clone https://github.com/N4SJAMK/teamboard-api    api
-git clone https://github.com/N4SJAMK/teamboard-client client
+# Default to N4SJAMK if no user is given.
+USER=${1-N4SJAMK}
+
+# Clone our repositories.
+git clone https://github.com/$USER/teamboard-io     io
+git clone https://github.com/$USER/teamboard-api    api
+git clone https://github.com/$USER/teamboard-client client
+
+# Set 'upstream' to the original repository.
+(cd io     && git remote add upstream https://github.com/N4SJAMK/teamboard-io)
+(cd api    && git remote add upstream https://github.com/N4SJAMK/teamboard-api)
+(cd client && git remote add upstream https://github.com/N4SJAMK/teamboard-client)
