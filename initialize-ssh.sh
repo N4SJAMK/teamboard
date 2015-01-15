1eb34c091bf23fc7003b1bfa7d78a69b901e8056
Teemu Laukka <teemulaukka@gmail.com>
2015-01-15 12:17:52 +0200

    Fix ssh urls in initialize-ssh

--- initialize-ssh.sh
+++ initialize-ssh.sh
@@ -4,11 +4,11 @@
 USER=${1-N4SJAMK}
 
 # Clone our repositories.
-git clone git@github.com/$USER/teamboard-io.git     io
-git clone git@github.com/$USER/teamboard-api.git    api
-git clone git@github.com/$USER/teamboard-client.git client
+git clone git@github.com:$USER/teamboard-io.git     io
+git clone git@github.com:$USER/teamboard-api.git    api
+git clone git@github.com:$USER/teamboard-client.git client
 
 # Set 'upstream' to the original repository.
-(cd io     && git remote add upstream git@github.com/N4SJAMK/teamboard-io.git)
-(cd api    && git remote add upstream git@github.com/N4SJAMK/teamboard-api.git)
-(cd client && git remote add upstream git@github.com/N4SJAMK/teamboard-client.git)
+(cd io     && git remote add upstream git@github.com:N4SJAMK/teamboard-io.git)
+(cd api    && git remote add upstream git@github.com:N4SJAMK/teamboard-api.git)
+(cd client && git remote add upstream git@github.com:N4SJAMK/teamboard-client.git)
