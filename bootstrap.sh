caae42a3d55dc3f0952762bc075cd009689cff5b
tanelih <taneli.hartikainen@jamk.fi>
2015-02-05 08:25:30 +0200

    Add some documentation about new client and other stuff...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#
+# Used to 'provision' the vagrant machine. Basically is run after the machine
+# is created so you don't have to worry about installing everything yourself.
 
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export LANGUAGE="en_US.UTF-8"
+
+# NodeJS and other stuff required by the 'api' and 'io' to run properly,
+# basically all sorts of MongoDBs, Redises etc...
 
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 
@@ -10,6 +16,7 @@ apt-get update
 apt-get install -y git nodejs build-essential
 apt-get install -y mongodb-server redis-server
 
-gem install sass
+# Build tools for development
 
-npm install --global --no-bin-links gulp bower
+gem install sass
+npm install -g --no-bin-links gulp bower
--- initialize-https.sh
+++ initialize-https.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Clones the repositories using 'https'. Use the 'initialize-ssh' if you prefer
+# to use 'ssh' in cloning the repositories.
+
 
 # Default to N4SJAMK if no user is given.
 USER=${1-N4SJAMK}
