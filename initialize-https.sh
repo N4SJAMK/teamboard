14cdf480bb3428d3304e4e795c4bb65b94251679
narsuman <narsuman@gmail.com>
2015-04-07 10:14:37 +0300

    Update initialize-https.sh
    Removed old client from setup

--- initialize-https.sh
+++ initialize-https.sh
@@ -10,11 +10,11 @@ USER=${1-N4SJAMK}
 # Clone our repositories.
 git clone https://github.com/$USER/teamboard-io           io
 git clone https://github.com/$USER/teamboard-api          api
-git clone https://github.com/$USER/teamboard-client       client
+# git clone https://github.com/$USER/teamboard-client       client
 git clone https://github.com/$USER/teamboard-client-react client-react
 
 # Set 'upstream' to the original repository.
 (cd io           && git remote add upstream https://github.com/N4SJAMK/teamboard-io)
 (cd api          && git remote add upstream https://github.com/N4SJAMK/teamboard-api)
-(cd client       && git remote add upstream https://github.com/N4SJAMK/teamboard-client)
+# (cd client       && git remote add upstream https://github.com/N4SJAMK/teamboard-client)
 (cd client-react && git remote add upstream https://github.com/N4SJAMK/teamboard-client-react)
