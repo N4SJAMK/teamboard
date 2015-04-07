74f90cddd770dfc78c837caa3fe37098e5fc79b5
narsuman <narsuman@gmail.com>
2015-04-07 10:17:12 +0300

    Update initialize-ssh.sh
    Removed old client from setup

--- initialize-ssh.sh
+++ initialize-ssh.sh
@@ -9,11 +9,11 @@ USER=${1-N4SJAMK}
 # Clone our repositories.
 git clone git@github.com:$USER/teamboard-io.git           io
 git clone git@github.com:$USER/teamboard-api.git          api
-git clone git@github.com:$USER/teamboard-client.git       client
+# git clone git@github.com:$USER/teamboard-client.git       client
 git clone git@github.com:$USER/teamboard-client-react.git client-react
 
 # Set 'upstream' to the original repository.
 (cd io           && git remote add upstream git@github.com:N4SJAMK/teamboard-io.git)
 (cd api          && git remote add upstream git@github.com:N4SJAMK/teamboard-api.git)
-(cd client       && git remote add upstream git@github.com:N4SJAMK/teamboard-client.git)
+# (cd client       && git remote add upstream git@github.com:N4SJAMK/teamboard-client.git)
 (cd client-react && git remote add upstream git@github.com:N4SJAMK/teamboard-client-react.git)
