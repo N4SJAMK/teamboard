46eb0a975cd8bbe8e9009c8ee63a1348883e0d6c
nikolauska <nikolauska1@gmail.com>
2015-05-28 15:35:00 +0300

    fixed error with linking to wrong repository

--- initialize-https.sh
+++ initialize-https.sh
@@ -12,11 +12,11 @@ git clone https://github.com/$USER/teamboard-io           io
 git clone https://github.com/$USER/teamboard-api          api
 # git clone https://github.com/$USER/teamboard-client       client
 git clone https://github.com/$USER/teamboard-client-react client-react
-git clone https://github.com/$USER/teamboard-client-react img
+git clone https://github.com/$USER/teamboard-img img
 
 # Set 'upstream' to the original repository.
-(cd io           && git remote add upstream https://github.com/N4SJAMK/teamboard-io)
-(cd api          && git remote add upstream https://github.com/N4SJAMK/teamboard-api)
+(cd io           && git remote add upstream https://github.com/N4SJAMK/teamboard-io.git)
+(cd api          && git remote add upstream https://github.com/N4SJAMK/teamboard-api.git)
 # (cd client       && git remote add upstream https://github.com/N4SJAMK/teamboard-client)
-(cd client-react && git remote add upstream https://github.com/N4SJAMK/teamboard-client-react)
-(cd img && git remote add upstream https://github.com/N4SJAMK/teamboard-img)
+(cd client-react && git remote add upstream https://github.com/N4SJAMK/teamboard-client-react.git)
+(cd img && git remote add upstream https://github.com/N4SJAMK/teamboard-img.git)
